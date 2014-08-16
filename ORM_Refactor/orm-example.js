d0a68b76a757518d574f7331f2d9060f3c2bc2ae
codengjon <1jonathan.ng@gmail.com>
2014-08-16 16:07:01 -0700

    added ORM refactor

--- ORM_Refactor/orm-example.js
+++ ORM_Refactor/orm-example.js
@@ -4,21 +4,25 @@
  */
 
 var Sequelize = require("sequelize");
-var sequelize = new Sequelize("chatter", "root", "");
+var sequelize = new Sequelize("chat", "root", "");
 /* TODO this constructor takes the database name, username, then password.
  * Modify the arguments if you need to */
 
 /* first define the data structure by giving property names and datatypes
  * See http://sequelizejs.com for other datatypes you can use besides STRING. */
 var User = sequelize.define('User', {
-  username: Sequelize.STRING
+  userName: Sequelize.STRING
+
 });
 
-var Message = sequelize.define('Message' {
-  userid: Sequelize.INTEGER,
-  text: Sequelize.STRING,
-  roomname: Sequelize.STRING
+var Message = sequelize.define('Message', {
+  // user_ID: Sequelize.INTEGER,
+  message_text: Sequelize.STRING,
+  roomName: Sequelize.STRING
 });
+
+User.hasMany(Message, {foreignKey: 'user_ID'})
+Message.belongsTo(User, {foreignKey: 'user_ID'})
 
 /* .sync() makes Sequelize create the database table for us if it doesn't
  *  exist already: */
@@ -26,17 +30,28 @@ User.sync().success(function() {
   /* This callback function is called once sync succeeds. */
 
   // now instantiate an object and save it:
-  var newUser = User.build({username: "Jean Valjean"});
+  var newUser = User.build({userName: "Jean Valjean"});
   newUser.save().success(function() {
 
     /* This callback function is called once saving succeeds. */
 
     // Retrieve objects from the database:
-    User.findAll({ where: {username: "Jean Valjean"} }).success(function(usrs) {
+    User.findAll({ where: {userName: "Jean Valjean"} }).success(function(usrs) {
       // This function is called back with an array of matches.
       for (var i = 0; i < usrs.length; i++) {
-        console.log(usrs[i].username + " exists");
+        console.log(usrs[i].userName + " exists");
       }
     });
   });
 });
+
+Message.sync().success(function(){
+  var newMessage = Message.build({message_text: 'hi', roomName: 'lobby', user_ID: 2});
+  newMessage.save().success(function(){
+    Message.findAll({where: {"User.userName": "Jean Valjean"}, include: [User]}).success(function(msgs){
+      for (var i = 0; i< msgs.length; i++){
+        console.log(msgs[i].message_text)
+      }
+    })
+  })
+})
